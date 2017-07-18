8a021a125e6e532dd194f550dd09ae54a73b184e
turangi <stuartfe@hotmail.com>
2017-07-18 13:02:30 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to sever');
-
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yep, works for me'
-  });
 });
 
 socket.on('disconnect', function () {
